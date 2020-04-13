97c68ebb2ea23426c7f3b2010634aa6b5b119aa4
Arsh Jain <arshjn@gmail.com>
2020-04-13 20:02:45 +0530

    Added GetStudent using Gateway and Controller

--- EnvironmentSetUp/Controllers/StudentController.cs
+++ EnvironmentSetUp/Controllers/StudentController.cs
@@ -1,24 +1,23 @@
-﻿using EnvironmentSetUp.Gateways;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EnvironmentSetUp.Models;
+using EnvironmentSetUp.Gateways;
 
 namespace EnvironmentSetUp.Controllers
 {
-    
     public class StudentController : ApiController
     {
-        StudentGateways sg = new StudentGateways();
+        private StudentGateway studentGateway = new StudentGateway();
         
-        [HttpGet]
-        [Route("api/Student/ID/{username}")]
-        public string GetStudentPassword(int username)
+        [HttpPost]
+        public Student GetStudent(int NNumber)
         {
-            return "Hello";
-            //return sg.GetStudent(username);
+            return studentGateway.GetStudent(NNumber);
         }
+
     }
 }
---
+++ EnvironmentSetUp/Gateways/StudentGateway.cs
@@ -0,0 +1,21 @@
+﻿using EnvironmentSetUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EnvironmentSetUp.Gateways
+{
+    public class StudentGateway
+    {
+        public Student GetStudent(int Id)
+        {
+            using (var ctx = new NovaRoomRegistrationTestEntities())
+            {
+                return ctx.Student.SqlQuery("Select * from Student where NNumber = " + Id).FirstOrDefault();
+            }
+        }
+
+    }
+}
